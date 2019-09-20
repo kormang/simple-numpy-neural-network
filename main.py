c4dbca344c2240f5ce6dee335c903390d14b1dbb
Marko Ivanovic <ivanovic.marko@yandex.com>
2019-09-20 04:07:08 +0300

    Added command-line arguments

--- main.py
+++ main.py
@@ -8,6 +8,31 @@ from cost import softmax_cross_entropy
 from utils import compute_classification_accuracy
 import pylab
 import warnings
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-d", "--dir", dest="datadir", default="../datasets/mnist/",
+                    help="path to directory containing data")
+parser.add_argument("-s", "--hidden-layer-sizes",
+                    dest="sizes", default="128",
+                    help="sizes of hidden layers separated by commas, e.g. '128,256'")
+parser.add_argument("-l", "--learning-rate",
+                    dest="learning_rate", default="0.05",
+                    help="learning rate")
+parser.add_argument("-m", "--max-iter",
+                    dest="max_iter", default="50",
+                    help="maximum number of iterations")
+parser.add_argument("-b", "--batch-size",
+                    dest="batch_size", default="600",
+                    help="batch size")
+
+args = parser.parse_args()
+
+datadir = args.datadir
+sizes = [int(s) for s in args.sizes.split(",")]
+learning_rate = float(args.learning_rate)
+max_iter = int(args.max_iter)
+batch_size = int(args.batch_size)
 
 # hide GLibc warning caused by matplotlib/pylab
 warnings.simplefilter("ignore")
@@ -19,8 +44,8 @@ warnings.simplefilter("ignore")
 # grad_check_classifier.grad_check(np.array([100, 130, 50, 300]), np.array([0, 1, 0, 0]))
 
 
-training_images, training_labels = loadMNIST("train", "../datasets/mnist/")
-test_images, test_labels = loadMNIST("t10k", "../datasets/mnist/")
+training_images, training_labels = loadMNIST("train", datadir)
+test_images, test_labels = loadMNIST("t10k", datadir)
 
 training_labels = to_hot_encoding(training_labels)
 test_labels = to_hot_encoding(test_labels)
@@ -37,9 +62,6 @@ training_images *= 1.0/255.0
 test_images *= 1.0/255.0
 
 np.random.seed(1345134)
-np.random.shuffle(training_images)
-np.random.seed(1345134)
-np.random.shuffle(training_labels)
 
 # training_images = training_images[0:1]
 # training_labels = training_labels[0:1]
@@ -49,11 +71,19 @@ np.random.shuffle(training_labels)
 # print (test_images.shape)
 # print (test_labels.shape)
 
-classifier = Classifier([
-    DenseLayer(training_images.shape[1], 128, ReLUActivation()),
-    SoftmaxCrossEntropyLayer(128, training_labels.shape[1])
-], softmax_cross_entropy)
-classifier.train(training_images, training_labels, max_iter=50, alpha=0.05, target_acc=0.99)
+layers = [DenseLayer(training_images.shape[1], sizes[0], ReLUActivation())]
+i = 1
+while i < len(sizes):
+    layers.append(DenseLayer(sizes[i-1], sizes[i], ReLUActivation()))
+    i += 1
+layers.append(SoftmaxCrossEntropyLayer(sizes[i-1], training_labels.shape[1]))
+
+classifier = Classifier(layers, softmax_cross_entropy)
+classifier.train(training_images, training_labels,
+    max_iter=max_iter,
+    learning_rate=learning_rate,
+    target_acc=0.99,
+    batch_size=batch_size)
 
 pylab.figure()
 pylab.plot(classifier.cost_history)
